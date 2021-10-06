6467a585870e49d3b1eb8094a9dcf12e3888cb17
PETER THOMAS OTIENO <40404367+otienopetert@users.noreply.github.com>
2021-10-06 01:21:19 -0700

    Update settings.py

--- vicatelweb/settings.py
+++ vicatelweb/settings.py
@@ -143,11 +143,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #SMTP Configuration
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = 'mail.vicatel.co.tz'
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'vicateltz@gmail.com'
-EMAIL_HOST_PASSWORD = 'vicatel2021'
+EMAIL_HOST_USER = 'peterotieno@vicatel.co.tz'
+EMAIL_HOST_PASSWORD = 'peterotieno@2021'
 
 
 django_heroku.settings(locals())
